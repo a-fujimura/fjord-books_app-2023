e1b0dac76f1b8330be5ad6713297886a06afd95c
a-fujimura <akr9915@gmail.com>
2025-05-28 11:34:31 +0900

    ja対応-i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # URL に locale を引き継ぐようにする
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
